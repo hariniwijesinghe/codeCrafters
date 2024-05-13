6b0e9ef8e5e6849c9fc4c643de7706eecaa747e7
harini <hariniwijesinghe@gmail.com>
2024-05-13 15:08:39 +1000

    Add Review UI Design

--- app/src/main/java/com/example/clothing/MainActivity.java
+++ app/src/main/java/com/example/clothing/MainActivity.java
@@ -1,25 +1,19 @@
 package com.example.clothing;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.clothing.databinding.ActivityMainBinding;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
-    DatabaseReference databaseReference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,13 +36,6 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         });
-
-        /*databaseReference= FirebaseDatabase.getInstance().getReference("This is the path");
-        databaseReference.setValue("Here there").addOnSuccessListener(new onSuccessListener<Void>(){
-            public void onSuccess(Void unused){
-                Toast.makeText(getApplicationContext(),"Success", Toast.LENGTH_SHORT).show();
-            }
-        })*/
     }
 
     private void replaceFragment(Fragment fragment) {
